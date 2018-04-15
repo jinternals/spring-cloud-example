7391f7b518f8b4bdc7b775b326e64276567b8f16
mradulpandey <pandeymradul@gmail.com>
2018-04-15 12:54:21 +0530

    Added spring ribbon with eureka

---
+++ spring-cloud-zuul-ribbon/README.md
@@ -0,0 +1,23 @@
+## Spring Cloud Example
+
+
+### Up running with docker :
+
+```
+mvn clean install
+mvn initialize docker:build
+docker-compose -f docker-compose.yml up -d --build
+```
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/jinternals/spring-cloud-example/master/spring-cloud-zuul/Zuul%20Diagram.png">
+</p>
+
+
+#
+### Links :
+
+* ###### blogs:
+
+    * [Zull How it works](https://github.com/Netflix/zuul/wiki/How-it-Works)
+
---
+++ spring-cloud-zuul-ribbon/spring-cloud-example-user-service/src/main/java/com/jinternals/spring/cloud/example/user/Application.java
@@ -0,0 +1,17 @@
+package com.jinternals.spring.cloud.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+
+}
