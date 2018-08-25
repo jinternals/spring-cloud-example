b27ec9fbc67524ba750ef64c324459e9e34a7c13
Mradul Pandey <pandeymradul@gmail.com>
2018-08-25 21:18:15 +0530

    update config

--- spring-cloud-zuul-ribbon-eureka/spring-cloud-example-user-service/src/main/java/com/jinternals/spring/cloud/example/user/Application.java
+++ spring-cloud-zuul-ribbon-eureka/spring-cloud-example-user-service/src/main/java/com/jinternals/spring/cloud/example/user/Application.java
@@ -1,12 +1,11 @@
 package com.jinternals.spring.cloud.example.user;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import static org.springframework.boot.SpringApplication.run;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
